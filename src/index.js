4500cb378d9cca7d1dc0d28d803ecc2fda5f1851
Alex Thomas <hello@cawfree.com>
2020-08-23 21:10:24 +0100

    chore: update return configuration to support wildcards

--- src/index.js
+++ src/index.js
@@ -29,20 +29,23 @@ const verifyMiddleware = ({ ...options }) => async (req, res, next) => {
   <script src="https://verify.sendwyre.com/js/pm-widget-init.js"></script>
   <script type="text/javascript">
     var redirect = {{{redirect}}}; 
+    function shouldRedirect(results) {
+      var q = redirect.includes("?") ? "&" : "?";
+      window.location.href = redirect + q + results;
+    }
     var handler = new WyrePmWidget({
       env: "{{{env}}}",
       onLoad: function() {
-        // In this example we open the modal immediately on load. More typically you might have the handler.open() invocation attached to a button.
         handler.open();
       },
       onSuccess: function(result) {
         if (redirect) {
-          window.location.href = redirect + "?wyreToken=" + btoa(result.publicToken);
+          shouldRedirect("wyreToken=" + btoa(result.publicToken));
         }
       },
       onExit: function(err) {
         if (redirect) {
-          window.location.href = redirect + "?wyreError=" + btoa(err.toString());
+          shouldRedirect("wyreError=" + btoa(err.toString()));
         }
       }
     });
